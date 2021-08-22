4d88d84495683384e3d338f992109cde04f02c77
MrDrummer <mrdrummer.programming@hotmail.com>
2021-08-23 00:31:23 +0100

    Handle errors with Discord & OBS

--- src/discord.ts
+++ src/discord.ts
@@ -2,7 +2,8 @@ import { Client, Intents } from "discord.js"
 import config from "./config.json"
 
 export default (): Promise<Client> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    let connected = false
     const client = new Client({
       intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 
@@ -19,9 +20,20 @@ export default (): Promise<Client> => {
       ]
     })
     client.login(config.discord.token)
+
+    const timeout = setTimeout(function () {
+      if (!connected) reject("Aborted connection to Discord.")
+    }, 20000)
     client.on("ready", () => {
       console.log(`Logged in as "${ client.user?.username }"!`)
+      botClient = client
+      connected = false
+      clearTimeout(timeout)
       resolve(client)
+    })
+
+    client.on("error", e => {
+      console.error("Discord had an error:", e)
     })
   })
 }
--- src/obs.ts
+++ src/obs.ts
@@ -1,10 +1,15 @@
 import ObsWebSocket from "obs-websocket-js"
 import config from "./config.json"
 export default async (): Promise<ObsWebSocket> => {
-  const obs = new ObsWebSocket()
-  await obs.connect({
-    address: `${ config.obs.hostname }:${ config.obs.port }`,
-    password: config.obs.password
-  })
-  return obs
+  try {
+    const obs = new ObsWebSocket()
+    await obs.connect({
+      address: `${ config.obs.hostname }:${ config.obs.port }`,
+      password: config.obs.password
+    })
+    return obs
+  } catch (e) {
+    console.error(e)
+    throw new Error("Couldn't connect to OBS. Is it running?")
+  }
 }
