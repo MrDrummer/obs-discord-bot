acc3121a10758e00d788f183d57bcd6a00b4b0c7
MrDrummer <mrdrummer.programming@hotmail.com>
2021-09-05 04:21:47 +0100

    fix log when switching to standby screen

--- src/commands/die.ts
+++ src/commands/die.ts
@@ -4,10 +4,9 @@ import { config } from "../config"
 import { setScene } from "../obs"
 
 export default async (args: yargs.CommandArgs, interaction?: CommandInteraction): Promise<void> => {
-  const argument = args.args._[0]
   try {
     await setScene(config.dieScene)
-    reply(args, `**scene:** ${ argument }`, interaction)
+    reply(args, `**scene:** ${ config.dieScene }`, interaction)
   } catch (e) {
     console.error("Error :", e)
     reply(args, "There was an error setting the scene.", interaction)
